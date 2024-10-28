1c128dedd03c661ac0e95e88e27634ac5ca16738
Tian Li Liu <tdl57@cornell.edu>
2024-10-27 23:06:22 -0400

    box format

--- nms.py
+++ nms.py
@@ -1,9 +1,9 @@
 import torch
 from iou import intersection_over_union
 
-def non_max_supression(
-        bboxes, iou_threshold, prob_threshold, box_format='corners'):
-    # predictions = [[1, 0.9, x1, y1, x2, y2]]
+def non_max_supression(bboxes, iou_threshold, prob_threshold, box_format='corners'):
+    # bboxes = [[1, 0.9, x1, y1, x2, y2], [], [], etc. etc.]
+    # each box is in format[class, confidence, x1, y1, x2, y2]
 
     assert type(bboxes) == list
     bboxes = [box for box in bboxes if box[1] > prob_threshold]
@@ -16,11 +16,7 @@ def non_max_supression(
         bboxes = [
             box for box in bboxes
             if box[0] != chosen_bbox[0] 
-            or intersection_over_union(
-                torch.tensor(chosen_bbox[2:]), 
-                torch.tensor(box[2:]), 
-                box_format=box_format
-            ) < iou_threshold
+            or intersection_over_union(torch.tensor(chosen_bbox[2:]), torch.tensor(box[2:]), box_format=box_format) < iou_threshold
         ]
 
         bboxes_after_nms.append(chosen_bbox)
